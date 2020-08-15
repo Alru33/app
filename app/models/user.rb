9477c0b5caa80b95c655ee297937667bb036ee53
Alru.33 <al-snd@yandex.ru>
2020-08-15 11:42:25 +0300

    Add validate length to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, length: { minimum: 2, maximum: 100}, presence: true
+  validates :email, length: { minimum: 2, maximum: 100}, presence: true
 end
